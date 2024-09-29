ee4abcbc7ee7f28b7fd2ec69dd5dabb0df4433fc
Sveta Baksichava <svetabaksichava@Alexeis-MacBook-Air.local>
2024-09-29 22:06:11 +0300

    feat: add main.tf

---
+++ .github/workflows/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 0.12"
+}
+
+provider "aws" {
+  region = "us-east-1"  
+}
+
+# Пример ресурса S3
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name"
+  acl    = "private"
+}
